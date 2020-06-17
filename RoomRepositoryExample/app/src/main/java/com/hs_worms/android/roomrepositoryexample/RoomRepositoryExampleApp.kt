4d3f15951b02037a307f71a439e661fe09411ccc
Stephan Kurpjuweit <stephan.kurpjuweit@thinking-apps.com>
2020-06-17 20:15:38 +0200

    Add RoomRepository project

---
+++ RoomRepositoryExample/app/src/main/java/com/hs_worms/android/roomrepositoryexample/RoomRepositoryExampleApp.kt
@@ -0,0 +1,13 @@
+package com.hs_worms.android.roomrepositoryexample
+
+import android.app.Application
+import com.hs_worms.android.roomrepositoryexample.model.ElementRepository
+
+class RoomRepositoryExampleApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        //ElementRepository.initialize(this)
+    }
+
+}
